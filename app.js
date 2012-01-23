13396355109f16cd2a5d15add06933ee0308b8a0
Dion Almaer <dion@almaer.com>
2012-01-23 13:10:22 -0800

    random service printeth

---
+++ app.js
@@ -0,0 +1,52 @@
+//
+// app.js -p [port || 3000]
+//
+
+//
+// Setup and Config
+//
+var express = require('express')
+  , argv = require('optimist').argv
+  , opts = parseOptions()
+
+//
+// Add new services here
+//
+
+// the holder for services plugins
+var services = []
+
+services.push(require('./services/helloworld.js'))
+services.push(require('./services/goodbyeworld.js'))
+
+//
+// Main
+//
+
+app = express.createServer()
+
+app.get('/', function(req, res) {
+  res.header('Content-Type', 'application/json');
+  (randomService())(req, res)
+});
+
+app.listen(opts.port)
+console.log("The Scan-O-Matic is up and listening on port: " + opts.port)
+
+//
+// Functions
+//
+
+// parse command line and return user options
+function parseOptions() {
+  var opts = {
+    port: argv.p || 3010
+  }
+
+  return opts;
+}
+
+// choose a random service
+function randomService() {
+  return services[Math.floor(services.length * Math.random())]
+}
