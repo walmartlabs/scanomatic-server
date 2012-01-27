7f82e58220f7b9f511465e8fdd63914d1a6f2c88
Anthony Marcar <anthony@grabbleapp.com>
2012-01-27 15:23:43 -0800

    file thing done

--- services/randomresponse.js
+++ services/randomresponse.js
@@ -3,14 +3,14 @@ var hits = 0;
 
 module.exports = function(req, res) {
 
-    poll(function() {
+    renderFile(function() {
 	if (hits % 10 == 0)
 	    randomResponse()
 	else
 	    res.send('nothing')
     });
 
-    function poll(onFileNotExists) {
+    function renderFile(onFileNotExists) {
 	fs.readFile("/tmp/message.txt", function(err, data) {
 	    if (err) onFileNotExists();
 	    res.render('world.html', 
@@ -20,6 +20,14 @@ module.exports = function(req, res) {
 		       });
 	    fs.unlink("/tmp/message.txt");
 	});
+    }
+
+    function randomResponse() {
+	res.render('world.html', 
+		   { title: 'Moo World', 
+		     dothis: "random", 
+		     barcode: "123"
+		   });
     }
 
     hits++;
