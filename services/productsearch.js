ab6aa9c9b27fd1ec6db2fd957be3d0d531f47087
Anthony Marcar <anthony@grabbleapp.com>
2012-01-27 19:51:46 -0800

    added fDb

--- services/productsearch.js
+++ services/productsearch.js
@@ -1,14 +1,46 @@
-var xmlrpc = require('xmlrpc')
+var xmlrpc = require('xmlrpc');
 var client = xmlrpc.createClient(
     { host: 'www.upcdatabase.com', 
       port: 80, 
       path: '/xmlrpc'
-    })
+    });
+
+require('./jDb');
+require('/etc/fDb');
 
 var rpcKey = 'e5a51bca97dca69e1fe8b1f1560bb507cfbb579e';
 
 module.exports = function(barcode, req, res) {
-    client.methodCall('lookup', [{'rpc_key': rpcKey, "upc":barcode}], handle);
+
+    var rec = jDb.barcode;
+    console.log(JSON.stringify(rec));
+
+    if (rec)
+	lookupWalmartCom(rec.id); // get correct ID
+    else
+	searchFDb();
+
+    function lookupWalmartCom(id) {
+	
+	res.render('product.html', 
+		   { name: "Walmart Com",
+		     country: "Country"})
+    }
+
+    function searchFdb() {
+	var rec = fdb.barcode;
+	
+	if (rec)
+	    res.render('product.html', 
+		       { name: "Fucked DB",
+			 country: "Fucked up country"})
+	else
+	    searchUpcDb();
+    }
+
+    function searchUpcDb() {
+	client.methodCall('lookup', [{'rpc_key': rpcKey, "upc":barcode}], handle);
+    }
 
     function handle(error, value) {
 	if (value.status == "fail")
@@ -24,3 +56,7 @@ module.exports = function(barcode, req, res) {
 		     
 }
 
+// drop leading 0
+// drop the checksum
+
+// product page http://upcdata.info/lookup/0038000765414/
